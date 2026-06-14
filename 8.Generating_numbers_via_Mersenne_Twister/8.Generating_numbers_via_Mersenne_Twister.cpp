e7e63e1562f64e2003e4589d43b11e660b3163ff
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-14 20:26:11 +0300

    Random Number generation via Mersenne Twister

---
+++ 8.Generating_numbers_via_Mersenne_Twister/8.Generating_numbers_via_Mersenne_Twister.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <random> // for std::mt19937
+
+int main()
+{
+	std::mt19937 mt{}; // Instantiate a 32-bit Mersenne Twister
+
+	// Print a bunch of random numbers
+	for (int count{ 1 }; count <= 40; ++count)
+	{
+		std::cout << mt() << '\t'; // generate a random number
+
+		// If we've printed 5 numbers, start a new row
+		if (count % 5 == 0)
+			std::cout << '\n';
+	}
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
