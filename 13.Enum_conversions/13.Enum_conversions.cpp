e77895c163cefaaf2d3b91509811e5d15eb51f54
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-15 22:28:45 +0300

    Enum conversion to and from stings

---
+++ 13.Enum_conversions/13.Enum_conversions.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string_view>
+
+enum Pet
+{
+    cat,   // 0
+    dog,   // 1
+    pig,   // 2
+    whale, // 3
+};
+
+constexpr std::string_view getPetName(Pet pet)
+{
+    switch (pet)
+    {
+    case cat:   return "cat";
+    case dog:   return "dog";
+    case pig:   return "pig";
+    case whale: return "whale";
+    default:    return "???";
+    }
+}
+
+int main()
+{
+    std::cout << "Enter a pet (0=cat, 1=dog, 2=pig, 3=whale): ";
+
+    int input{};
+    std::cin >> input; // input an integer
+
+    if (input < 0 || input > 3)
+        std::cout << "You entered an invalid pet\n";
+    else
+    {
+        Pet pet{ static_cast<Pet>(input) }; // static_cast our integer to a Pet
+        std::cout << "You entered: " << getPetName(pet) << '\n';
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
