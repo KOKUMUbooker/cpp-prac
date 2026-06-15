ea7d094bf3f78915c8ef22eb9feb401e76b241fe
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-15 11:44:41 +0300

    Pass by lvalue reference sample

---
+++ 12.Pass_by_LValue_reference/12.Pass_by_LValue_reference.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+// Pass by reference offers a way to avoid making an expensive copy of an argument when calling a function 
+void printValue(std::string& y) // type changed to std::string&
+{
+    std::cout << y << '\n';
+} // y is destroyed here
+
+int main()
+{
+    std::string x{ "Hello, world!" };
+
+    printValue(x); // x is now passed by reference into reference parameter y (inexpensive)
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
