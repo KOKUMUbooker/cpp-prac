0228f1cc62321d4f2f9a3fa350e7759d90614ddc
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-15 12:44:40 +0300

    Pointer to const value

---
+++ 12.Pointer_to_const_value/12.Pointer_to_const_value.cpp
@@ -0,0 +1,24 @@
+// Pointer to a const - is a (non-const) pointer that points to a constant value.
+int main()
+{
+    const int x{ 5 };
+    const int* ptr{ &x }; // okay: ptr is pointing to a "const int"
+
+    // *ptr = 6; // not allowed: we can't change a const value
+
+    const int y{ 6 };
+    ptr = &y; // okay: ptr now points at const int y
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
