b7dc689eb3481c4c3511aba1bcb4fdea2acbb218
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-15 23:07:57 +0300

    Scoped enums

---
+++ 13.Scoped_enumerations/13.Scoped_enumerations.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <utility> // for std::to_underlying() (C++23)
+
+int main()
+{
+    enum class Color // // "enum class" defines this as a scoped enum rather than an unscoped enum
+    {
+        red,  // red is considered part of Color's scope region
+        blue,
+    };
+
+    Color color{ Color::blue };
+
+    // std::cout << color << '\n'; // won't work, because there's no implicit conversion to int
+    std::cout << static_cast<int>(color) << '\n';   // explicit conversion to int, will print 1
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
