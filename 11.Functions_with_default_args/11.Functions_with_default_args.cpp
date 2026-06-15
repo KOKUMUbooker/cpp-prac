6918e811786ed68edd720ba2e2c33b45fc451d26
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-15 10:23:08 +0300

    Functions with default arguments

---
+++ 11.Functions_with_default_args/11.Functions_with_default_args.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Either place the default values in the forward declaration or in the function definition - NOT both
+void print(int x,int y,int z); // forward declaration
+
+// function definition
+void print(int x = 10, int y = 20, int z = 30)
+{
+    std::cout << "Values: " << x << " " << y << " " << z << '\n';
+}
+
+int main()
+{
+    print(1, 2, 3); // all explicit arguments
+    print(1, 2); // rightmost argument defaulted
+    print(1); // two rightmost arguments defaulted
+    print(); // all arguments defaulted
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
