655e4c2a33821d56287919b0b26a6325ad5964bb
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-15 12:19:05 +0300

    Pointers intro

---
+++ 12.Pointer_intro/12.Pointer_intro.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    int x{ 5 };
+    int* ptr{ &x }; // initialize ptr with address of variable x
+
+    std::cout << x << '\n';    // print x's value
+    std::cout << *ptr << '\n'; // print the value at the address that ptr is holding (x's address)
+    std::cout << "&x : " << &x << " , ptr : " << ptr << '\n'; // print the address of what ptr is pointing to
+
+    *ptr = 6; // The object at the address held by ptr (x) assigned value 6 (note that ptr is dereferenced here)
+
+    std::cout << x << '\n';
+    std::cout << *ptr << '\n'; // print the value at the address that ptr is holding (x's address)
+    std::cout << "Updated ptr : " << ptr << '\n'; // print the address of what ptr is pointing to
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
