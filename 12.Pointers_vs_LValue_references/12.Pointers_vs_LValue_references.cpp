3c9c358b2a516a507dd275392c3d6ff7d3a61393
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-15 12:25:38 +0300

    Pointers vs LValue references

---
+++ 12.Pointers_vs_LValue_references/12.Pointers_vs_LValue_references.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+// Pointers are very similar to LValue references with the exception that
+// pointers can change what the reference while LValue references can't
+// change what they reference ie (they can't be reseated)
+int main()
+{
+    int x{ 5 };
+    int& ref{ x };  // get a reference to x
+    int* ptr{ &x }; // get a pointer to x
+
+    std::cout << x;
+    std::cout << ref;  // use the reference to print x's value (5)
+    std::cout << *ptr << '\n'; // use the pointer to print x's value (5)
+
+    ref = 6; // use the reference to change the value of x
+    std::cout << x;
+    std::cout << ref;  // use the reference to print x's value (6)
+    std::cout << *ptr << '\n'; // use the pointer to print x's value (6)
+
+    *ptr = 7; // use the pointer to change the value of x
+    std::cout << x;
+    std::cout << ref;  // use the reference to print x's value (7)
+    std::cout << *ptr << '\n'; // use the pointer to print x's value (7)
+
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
