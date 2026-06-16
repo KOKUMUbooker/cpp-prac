cd1d8f349c17530101de2fe23146bb7205ac7d6f
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-16 11:17:40 +0300

    Templates in structs

---
+++ 13.Class_templates/13.Class_templates.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+template <typename T, typename U>
+struct Pair
+{
+    T first{};
+    U second{};
+};
+
+template <typename T, typename U>
+void print(Pair<T, U> p)
+{
+    std::cout << '[' << p.first << ", " << p.second << ']';
+}
+
+int main()
+{
+    Pair<int, double> p1{ 1, 2.3 }; // a pair holding an int and a double
+    Pair<double, int> p2{ 4.5, 6 }; // a pair holding a double and an int
+    Pair<int, int> p3{ 7, 8 };      // a pair holding two ints
+
+    print(p2);
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
