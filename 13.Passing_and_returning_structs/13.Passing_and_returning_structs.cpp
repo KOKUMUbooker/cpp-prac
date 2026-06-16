000a96f9dffe12e5cca321b99900704c1e9e74c3
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-16 10:34:40 +0300

    Passing and returning structs

---
+++ 13.Passing_and_returning_structs/13.Passing_and_returning_structs.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+struct Employee
+{
+    int id{};
+    int age{};
+    double wage{};
+};
+
+struct Point3d
+{
+    double x{ 0.0 };
+    double y{ 0.0 };
+    double z{ 0.0 };
+};
+
+void printEmployee(const Employee& employee) // note pass by reference here
+{
+    std::cout << "ID:   " << employee.id << '\n';
+    std::cout << "Age:  " << employee.age << '\n';
+    std::cout << "Wage: " << employee.wage << '\n';
+}
+
+Point3d getZeroPoint()
+{
+    return Point3d{ 0.0, 0.0, 0.0 }; // return an unnamed Point3d
+}
+
+int main()
+{
+    // Print Joe's information
+    printEmployee(Employee{ 14, 32, 24.15 }); // construct a temporary Employee to pass to function (type explicitly specified) (preferred)
+
+    std::cout << '\n';
+
+    // Print Frank's information
+    printEmployee({ 15, 28, 18.27 }); // construct a temporary Employee to pass to function (type deduced from parameter)
+
+
+    Point3d zero{ getZeroPoint() };
+
+    if (zero.x == 0.0 && zero.y == 0.0 && zero.z == 0.0)
+        std::cout << "The point is zero\n";
+    else
+        std::cout << "The point is not zero\n";
+
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
