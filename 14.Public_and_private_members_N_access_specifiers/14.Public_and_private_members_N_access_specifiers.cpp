0cfd4763b8e11464b02a0f2f544908a48a642a48
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-16 16:33:23 +0300

    Public n private members and access specifiers

---
+++ 14.Public_and_private_members_N_access_specifiers/14.Public_and_private_members_N_access_specifiers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class Date
+{
+    // Any members defined here would default to private
+
+public: // here's our public access specifier
+
+    void print() const // public due to above public: specifier
+    {
+        // members can access other private members
+        std::cout << m_year << '/' << m_month << '/' << m_day;
+    }
+
+private: // here's our private access specifier
+
+    int m_year{ 2026 };  // private due to above private: specifier
+    int m_month{ 6 }; // private due to above private: specifier
+    int m_day{ 16 };   // private due to above private: specifier
+};
+
+int main()
+{
+    Date d{};
+    d.print();  // okay, main() allowed to access public members
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
