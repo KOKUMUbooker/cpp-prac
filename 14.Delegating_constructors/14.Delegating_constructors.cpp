715a690c123ce6ab6c767ba6c1d98393f07b0a20
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-17 10:16:55 +0300

    Delegating constructors

---
+++ 14.Delegating_constructors/14.Delegating_constructors.cpp
@@ -0,0 +1,42 @@
+// Constructors are allowed to delegate (transfer responsibility for) initialization to another constructor from the same class type. 
+// This process is sometimes called constructor chaining and such constructors are called delegating constructors.
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Employee
+{
+private:
+    std::string m_name{ "???" };
+    int m_id{ 0 };
+
+public:
+    Employee(std::string_view name)
+        : Employee{ name, 0 } // delegate initialization to Employee(std::string_view, int) constructor
+    {
+    }
+
+    Employee(std::string_view name, int id)
+        : m_name{ name }, m_id{ id } // actually initializes the members
+    {
+        std::cout << "Employee " << m_name << " created\n";
+    }
+
+};
+
+int main()
+{
+    Employee e1{ "James" };
+    Employee e2{ "Dave", 42 };
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
