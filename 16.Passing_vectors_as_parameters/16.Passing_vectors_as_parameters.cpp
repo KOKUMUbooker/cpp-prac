453bbdadad59b6846b11183f7639c29065c2ef53
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-17 14:54:47 +0300

    Passing vectors as parameters

---
+++ 16.Passing_vectors_as_parameters/16.Passing_vectors_as_parameters.cpp
@@ -0,0 +1,28 @@
+//  Passing a std::vector by value makes an expensive copy. 
+// Therefore, we typically pass std::vector by (const) reference to avoid such copies.
+#include <iostream>
+#include <vector>
+
+void passByRef(const std::vector<int>& arr) // we must explicitly specify <int> here
+{
+    std::cout << arr[0] << '\n';
+}
+
+int main()
+{
+    std::vector primes{ 2, 3, 5, 7, 11 };
+    passByRef(primes);
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
