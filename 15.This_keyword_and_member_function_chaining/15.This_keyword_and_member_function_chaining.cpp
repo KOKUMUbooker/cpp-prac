f14a05b13e69764ca20274e5314c8ce6ff3cf400
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-17 11:32:26 +0300

    This keyword and member function chaining

---
+++ 15.This_keyword_and_member_function_chaining/15.This_keyword_and_member_function_chaining.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+class Calc
+{
+private:
+    int m_value{};
+
+public:
+    Calc& add(int value) { m_value += value; return *this; }
+    Calc& sub(int value) { m_value -= value; return *this; }
+    Calc& mult(int value) { m_value *= value; return *this; }
+
+    int getValue() const { return m_value; }
+
+    void reset() { *this = {}; }
+};
+
+
+int main()
+{
+    Calc calc{};
+    calc.add(5).sub(3).mult(4);
+
+    std::cout << calc.getValue() << '\n'; // prints 8
+
+    calc.reset();
+
+    std::cout << calc.getValue() << '\n'; // prints 0
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
