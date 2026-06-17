df51e642d99af231958a660061ac30e0bf32b192
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-17 10:27:45 +0300

    Temporary class objects

---
+++ 14.Temporary_class_objects/14.Temporary_class_objects.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class IntPair
+{
+private:
+    int m_x{};
+    int m_y{};
+
+public:
+    IntPair(int x, int y)
+        : m_x{ x }, m_y{ y }
+    {
+    }
+
+    int x() const { return m_x; }
+    int y() const { return m_y; }
+};
+
+void print(IntPair p)
+{
+    std::cout << "(" << p.x() << ", " << p.y() << ")\n";
+}
+
+int main()
+{
+    // Case 1: Pass variable
+    IntPair p{ 3, 4 };
+    print(p);
+
+    // Case 2: Construct temporary IntPair and pass to function
+    print(IntPair{ 5, 6 });
+
+    // Case 3: Implicitly convert { 7, 8 } to a temporary Intpair and pass to function
+    print({ 7, 8 });
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
