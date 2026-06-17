970d0dcc2b7d600a49b769adf0ac01de78bd35a2
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-17 11:51:09 +0300

    Destructors

---
+++ 15.Destructors/15.Destructors.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class Simple
+{
+private:
+    int m_id{};
+
+public:
+    Simple(int id)
+        : m_id{ id }
+    {
+        std::cout << "Constructing Simple " << m_id << '\n';
+    }
+
+    ~Simple() // here's our destructor
+    {
+        std::cout << "Destructing Simple " << m_id << '\n';
+    }
+
+    int getID() const { return m_id; }
+};
+
+int main()
+{
+    // Allocate a Simple
+    Simple simple1{ 1 };
+    {
+        Simple simple2{ 2 };
+    } // simple2 dies here
+
+    return 0;
+} // simple1 dies here
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
