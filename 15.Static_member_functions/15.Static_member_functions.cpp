69974466929c1536635d5aa8e69567589a53ad33
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-17 12:25:42 +0300

    Static member functions

---
+++ 15.Static_member_functions/15.Static_member_functions.cpp
@@ -0,0 +1,33 @@
+// Static member functions lack access to the this keyword
+#include <iostream>
+
+class IDGenerator
+{
+private:
+    static inline int s_nextID{ 1 };
+
+public:
+    static int getNextID(); // Here's the declaration for a static function
+};
+
+// Here's the definition of the static function outside of the class.  Note we don't use the static keyword here.
+int IDGenerator::getNextID() { return s_nextID++; }
+
+int main()
+{
+    for (int count{ 0 }; count < 5; ++count)
+        std::cout << "The next ID is: " << IDGenerator::getNextID() << '\n';
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
