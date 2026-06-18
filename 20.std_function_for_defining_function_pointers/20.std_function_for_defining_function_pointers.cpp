b6ad420b728e1992b85edad3c50351b7a8f9506b
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-18 11:39:09 +0300

    Using std::function to define function pointers

---
+++ 20.std_function_for_defining_function_pointers/20.std_function_for_defining_function_pointers.cpp
@@ -0,0 +1,23 @@
+#include <functional>
+#include <iostream>
+
+int foo()
+{
+    return 5;
+}
+
+int goo()
+{
+    return 6;
+}
+
+int main()
+{
+    std::function<int()> fcnPtr{ &foo }; // declare function pointer that returns an int and takes no parameters
+    fcnPtr = &goo; // fcnPtr now points to function goo
+    std::cout << fcnPtr() << '\n'; // call the function just like normal
+
+    std::function fcnPtr2{ &foo }; // can also use CTAD to infer template arguments
+
+    return 0;
+}
