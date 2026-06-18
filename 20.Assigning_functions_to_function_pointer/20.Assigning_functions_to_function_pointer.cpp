a35dd6a0db3e77a590b9583545e544430cd7d597
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-18 11:11:10 +0300

    Calling a function via its pointer

--- 20.Assigning_functions_to_function_pointer/20.Assigning_functions_to_function_pointer.cpp
+++ 20.Assigning_functions_to_function_pointer/20.Assigning_functions_to_function_pointer.cpp
@@ -8,10 +8,21 @@ int goo()
     return 6;
 }
 
+int fooWithArgs(int x)
+{
+    return x;
+}
+
 int main()
 {
+    // =============== Assigning a function to a pointer
     int (*fcnPtr)() { &foo }; // fcnPtr points to function foo
     fcnPtr = &goo; // fcnPtr now points to function goo
+
+    // =============== Calling a function via its pointer
+    int (*fcnWithArgsPtr)(int) { &fooWithArgs }; // Initialize fcnWithArgsPtr with function fooWithArgs
+    if (fcnWithArgsPtr) // make sure fcnPtr isn't a null pointer
+        fcnWithArgsPtr(5); // otherwise this will lead to undefined behavior
 
     return 0;
 }
