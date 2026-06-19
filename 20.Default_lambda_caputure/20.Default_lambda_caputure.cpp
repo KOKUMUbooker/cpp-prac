840415089dcd7464b5fe8c0d0341fe046a22b4d3
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-19 09:42:09 +0300

    Default lambda capture

---
+++ 20.Default_lambda_caputure/20.Default_lambda_caputure.cpp
@@ -0,0 +1,35 @@
+// A default capture captures all variables that are mentioned in the lambda.
+// Variables not mentioned in the lambda are not captured if a default capture is used.
+
+// To capture all used variables by value, use a capture value of = .
+// To capture all used variables by reference, use a capture value of& .
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+int main()
+{
+    std::array areas{ 100, 25, 121, 40, 56 };
+
+    int width{};
+    int height{};
+
+    std::cout << "Enter width and height: ";
+    std::cin >> width >> height;
+
+    auto found{ std::find_if(areas.begin(), areas.end(),
+                             [=](int knownArea) { // will default capture width and height by value
+                               return width * height == knownArea; // because they're mentioned here
+                             }) };
+
+    if (found == areas.end())
+    {
+        std::cout << "I don't know this area :(\n";
+    }
+    else
+    {
+        std::cout << "Area found :)\n";
+    }
+
+    return 0;
+}
